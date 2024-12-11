31e945b3d110bf8d569eb4de9e70e8c61a36fa5e
capellett <c.alex.pellett@gmail.com>
2024-12-10 20:39:43 -0500

    updates

--- app.R
+++ app.R
@@ -1,6 +1,8 @@
 ## This bit is used by shinylive to run the app
 
-pkgload::load_all(".") # Load the package
+# pkgload::load_all(".") # Load the package
+load("data/pop_proj.rda")
+source("R/data_explorer_app.R")
 scpopulation_app()
 
 ## run this in the console to export the app to docs/articles
